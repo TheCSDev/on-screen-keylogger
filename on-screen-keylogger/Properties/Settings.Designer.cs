3384987a65c855684f4aead1718a77e4875df876
TheCSDev <thecsdev@gmail.com>
2022-02-03 17:01:26 +0100

    Release v1.5

--- on-screen-keylogger/Properties/Settings.Designer.cs
+++ on-screen-keylogger/Properties/Settings.Designer.cs
@@ -58,5 +58,17 @@ namespace on_screen_keylogger.Properties {
                 this["ShowMenu"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool LimitResizeRequests {
+            get {
+                return ((bool)(this["LimitResizeRequests"]));
+            }
+            set {
+                this["LimitResizeRequests"] = value;
+            }
+        }
     }
 }
